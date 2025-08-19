1fef65043e21b9d395e22e6c27a28f57509e5cc1
valmtv <valerii.matviiv@gmail.com>
2025-08-19 22:51:22 +0200

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,12 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
  });
   
 // Get book details based on author
