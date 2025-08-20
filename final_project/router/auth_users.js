0461734844362c86f9a526110bf9376243abd353
valmtv <valerii.matviiv@gmail.com>
2025-08-20 12:07:53 +0200

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -63,9 +63,28 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
   const username = jwt.decode(req.session.authorization.accessToken).username;
   books[isbn].reviews[username] = review;
-  return res.status(200)
-    .json({message: "Review added successfully", book: books[isbn]});
+  return res.status(200).json({
+    message: "Review added successfully",
+    book: books[isbn]
+  });
 
+});
+
+
+// Delete a review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = jwt.decode(req.session.authorization.accessToken).username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({message: "Book not found"});
+  }
+  if (books[isbn].reviews && books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res.status(200).json({message: "Review deleted successfully"});
+  } else {
+    return res.status(404).json({message: "Review not found"});
+  }
 });
 
 module.exports.authenticated = regd_users;
